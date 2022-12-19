70644e40e509865e37adf1805e497fce38c0d845
Alysson Menezes <almenezes912@gmail.com>
2022-12-19 15:35:07 -0300

    configurando projeto em viewCode

---
+++ tablewView-ViewCode/tablewView-ViewCode/Menu/ViewController/MenuViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MenuViewController.swift
+//  tablewView-ViewCode
+//
+//  Created by ALYSSON DODO on 19/12/22.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
